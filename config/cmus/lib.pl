c6d5e2f9b1f2981be008f4db3fcb36be06506ae3
OdiousImp2604 <74424076+OdiousImp2604@users.noreply.github.com>
2021-02-22 13:49:51 +0000

    Add files via upload

---
+++ config/cmus/lib.pl
@@ -0,0 +1,68 @@
+/home/aryan/Music/Unknown Artist-Mood (feat. iann dior).m4a
+/home/aryan/Music/Unknown Artist-808.m4a
+/home/aryan/Music/Unknown Artist-Kennington Where It Started.m4a
+/home/aryan/Music/Unknown Artist-On Deck (feat. Young Thug).m4a
+/home/aryan/Music/Unknown Artist-Advice.m4a
+/home/aryan/Music/Unknown Artist-My Oh My (feat. DaBaby).m4a
+/home/aryan/Music/Unknown Artist-Go Crazy.m4a
+/home/aryan/Music/Unknown Artist-Ramsay.m4a
+/home/aryan/Music/Unknown Artist-Amazing Grace.m4a
+/home/aryan/Music/Unknown Artist-GO.m4a
+/home/aryan/Music/Unknown Artist-Bad Vibes.m4a
+/home/aryan/Music/Unknown Artist-BOP.m4a
+/home/aryan/Music/Unknown Artist-TOES (feat. Lil Baby & Moneybagg Yo).m4a
+/home/aryan/Music/Unknown Artist-ROCKSTAR (feat. Roddy Ricch).m4a
+/home/aryan/Music/Unknown Artist-Circles.m4a
+/home/aryan/Music/Unknown Artist-Daily Duppy (feat. GRM Daily).m4a
+/home/aryan/Music/Unknown Artist-Daily Duppy!.m4a
+/home/aryan/Music/Unknown Artist-Behind Barz (Bonus).m4a
+/home/aryan/Music/Unknown Artist-Without Me.m4a
+/home/aryan/Music/Unknown Artist-Godzilla (feat. Juice WRLD).m4a
+/home/aryan/Music/Unknown Artist-Mockingbird.m4a
+/home/aryan/Music/Unknown Artist-Good Guy (feat. Jessie Reyez).m4a
+/home/aryan/Music/Unknown Artist-Those Kinda Nights (feat. Ed Sheeran).m4a
+/home/aryan/Music/Unknown Artist-Stepdad.m4a
+/home/aryan/Music/Unknown Artist-Not Afraid.m4a
+/home/aryan/Music/Unknown Artist-Love the Way You Lie (feat. Rihanna).m4a
+/home/aryan/Music/Unknown Artist-Beautiful.m4a
+/home/aryan/Music/Unknown Artist-Walk On Water (feat. Beyoncé).m4a
+/home/aryan/Music/Unknown Artist-River (feat. Ed Sheeran).m4a
+/home/aryan/Music/Unknown Artist-Guts Over Fear (feat. Sia).m4a
+/home/aryan/Music/Unknown Artist-No Words (feat. Eminem & Big Proof).m4a
+/home/aryan/Music/Unknown Artist-Superman.m4a
+/home/aryan/Music/Unknown Artist-Cleanin' Out My Closet.m4a
+/home/aryan/Music/Unknown Artist-'Till I Collapse (feat. Nate Dogg).m4a
+/home/aryan/Music/Unknown Artist-Stan (feat. Dido).m4a
+/home/aryan/Music/Unknown Artist-The Way I Am.m4a
+/home/aryan/Music/Unknown Artist-The Real Slim Shady.m4a
+/home/aryan/Music/Unknown Artist-Rap God.m4a
+/home/aryan/Music/Unknown Artist-The Monster (feat. Rihanna).m4a
+/home/aryan/Music/Unknown Artist-Just Don't Give a Fuck.m4a
+/home/aryan/Music/Unknown Artist-When I'm Gone.m4a
+/home/aryan/Music/Unknown Artist-Ain't It Different (feat. AJ Tracey & Stormzy).m4a
+/home/aryan/Music/Unknown Artist-Once in a While.m4a
+/home/aryan/Music/Unknown Artist-Only You Freestyle.m4a
+/home/aryan/Music/Unknown Artist-Wishing Well.m4a
+/home/aryan/Music/Unknown Artist-Come & Go.m4a
+/home/aryan/Music/Unknown Artist-I AM.m4a
+/home/aryan/Music/Unknown Artist-6locc 6a6y (feat. NLE Choppa).m4a
+/home/aryan/Music/Unknown Artist-Project Dreams.m4a
+/home/aryan/Music/Unknown Artist-Taco Tuesday.m4a
+/home/aryan/Music/Unknown Artist-Ballin’.m4a
+/home/aryan/Music/Unknown Artist-Dun out Ere.m4a
+/home/aryan/Music/Unknown Artist-Top Shotta Flow.m4a
+/home/aryan/Music/Unknown Artist-Narrow Road (feat. Lil Baby).m4a
+/home/aryan/Music/Unknown Artist-Walk Em Down (feat. Roddy Ricch).m4a
+/home/aryan/Music/Unknown Artist-Through da Storm.m4a
+/home/aryan/Music/Unknown Artist-Purple.m4a
+/home/aryan/Music/Unknown Artist-Polo G 21 (19).m4a
+/home/aryan/Music/Unknown Artist-Epidemic.m4a
+/home/aryan/Music/Unknown Artist-Martin and Gina.m4a
+/home/aryan/Music/Heartless.m4a
+/home/aryan/Music/Beautiful Pain (Losin My Mind).m4a
+/home/aryan/Music/Unknown Artist-What You Know Bout Love.m4a
+/home/aryan/Music/Unknown Artist-UCLA.m4a
+/home/aryan/Music/Unknown Artist-Down Below.m4a
+/home/aryan/Music/Unknown Artist-Tip Toe (feat. A Boogie wit da Hoodie).m4a
+/home/aryan/Music/Unknown Artist-Own It (feat. Ed Sheeran & Burna Boy).m4a
+/home/aryan/Music/Unknown Artist-Jet Ski.m4a
